dfd0a65fea0f56fd48798edc12d90bad3c4e4d90
DC <dc@example.com>
2025-10-12 21:29:03 +0300

    init

---
+++ MedSupply/MedSupply/MedSupplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedSupplyApp.swift
+//  MedSupply
+//
+//  Created by Oleksandr Kataskin on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MedSupplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
